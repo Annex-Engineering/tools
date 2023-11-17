a1f17b8707d78da37c5ed095b2922d48147d8ce0
Lasse Dalegaard <dalegaard@gmail.com>
2023-11-17 15:53:17 +0100

    Add front page

---
+++ _frontpage/vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  server: { useFullPath: true },
+  root: "src",
+});
--- build.sh
+++ build.sh
@@ -9,3 +9,4 @@ rm -rf dist/
 mkdir -p dist/
 
 (cd beacon_plotter && bun install --dev && bun run build --base "/beacon_plotter" --outDir "$SELFDIR/dist/beacon_plotter")
+(cd _frontpage && bun install --dev && bun run build --base "/" --outDir "$SELFDIR/dist")
